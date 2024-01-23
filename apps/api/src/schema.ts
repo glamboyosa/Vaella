59bf9a7724715e62235f9130737c42357acd6e63
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-01-23 20:47:40 +0100

    mutation handler for email

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,6 +1,6 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const email = sqliteTable("email", {
+export const emailSchema = sqliteTable("email", {
   id: integer("id").primaryKey(),
   email: text("email").notNull().unique(),
 });
